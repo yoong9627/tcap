8f65b039ee49594d5e968ed5ec7b3c8ffdc652e8
Yong Bum Yoo <yongy0221@gmail.com>
2017-11-12 14:16:07 -0800

    fix mailer on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,17 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 25,
+    domain: "tcp2-shy.herokuapp.com",
+    authentication: "login",
+    enable_starttls_auto: true,
+    user_name: "yongy0221@gmail.com",
+    password: "Dydqja123"
+  }s
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
